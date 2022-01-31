3bd46d3932fa07fdd2a421898a8ff550f18e0e33
Prasad Karmalkar <prasadkarmalkar2020@gmail.com>
2022-01-31 15:51:16 +0530

    initial

---
+++ chat.model.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const chatSchema = Schema({
+    user_id:{
+        type:Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    username:{
+        type:String
+    },
+    profile_image:{
+        type:String
+    },
+    type:{
+        type:String,
+        required:true,
+        default:'text'
+    },
+    message:{
+        type:String,
+    },
+    url:{
+        type:String
+    },
+    reply_to:{
+        type:Schema.Types.ObjectId,
+        ref:'User'
+    },
+    createdAt:{
+        type:Date,
+        required:true
+    }
+})
+
+  const roomSchema = Schema({
+      room_admin:{
+        type:Schema.Types.ObjectId,
+        ref:'User',
+        required:true,
+      },
+      chats:[chatSchema],
+  })
+
+  
+const Room = mongoose.model('Room', roomSchema);
+module.exports = Room;
---
+++ index.js
@@ -0,0 +1,64 @@
+const Room = require('../dbeats-backend/models/chat.model');
+const User = require('../dbeats-backend/models/user.model');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 5000
+const io = require('socket.io')(PORT,
+    {
+      cors: {
+        origin: "*",
+        transports: ['websocket'],
+        credentials: true
+    },
+    allowEIO3: true
+    }
+  );
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+  io.on('connection', (socket) => {
+    socket.on('joinroom', async ({ user_id, room_id }) => {
+      const user = await User.findById(user_id);
+      const room = await Room.findOne({ room_admin: room_id });
+      const room_user = await User.findById(room_id);
+      if (room && user) {
+        await socket.join(room_user._id.toString());
+        socket.emit('init', room.chats);
+      } else if (user && room_user) {
+        const withoutroom = await Room.create({
+          room_admin: room_id,
+          chats: [],
+        });
+        socket.emit('init', withoutroom.chats);
+      }
+    });
+    socket.on('chatMessage', async ({ chat, room_admin }) => {
+      try {
+        const msgId = new mongoose.Types.ObjectId();
+        const c = {
+          _id: msgId,
+          user_id: chat.user_id,
+          username: chat.username,
+          profile_image: chat.profile_image,
+          type: chat.type,
+          message:chat.message,
+          createdAt:chat.createdAt
+        }
+        Room.findOneAndUpdate(
+          { room_admin: room_admin },
+          {
+            $push: {
+              chats: c,
+            },
+          },
+          function (error, success) {
+            if (error) {
+              console.log(error);
+            } else {
+              io.to(room_admin.toString()).emit('message', c);
+            }
+          },
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  });
